df0490f6408f22f4ced0cc885e367d6c8311b082
Spacecodee <spacecodee@gmail.com>
2022-06-24 20:15:57 -0500

    module variables in Java

---
+++ src/main/java/com/spacecodee/AppMain.java
@@ -0,0 +1,8 @@
+package com.spacecodee;
+
+public class AppMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/com/spacecodee/javabasic/Variables.java
@@ -0,0 +1,38 @@
+package com.spacecodee.javabasic;
+
+public class Variables {
+    //Java => nomenclatura camelCase
+    //variables => area ✅ | Area ❌ | AREA ❌ || firstName ✅ | firstname | FirstName
+    //functions => public void sumar(); ✅ | Sumar() ❌ | SUMAR(); ❌ || quieroSumar();
+    //Classes => Product ✅ | product ❌ | PRODUCT ❌ || UserRoles ✅ | USERroles | userRoleS
+    //Constants => AGE | age | AgE
+
+    //tipos de datos
+    /*
+     *=> primitivas
+     * numeros enteros => byte, int, long
+     * numeros decimales => float, double
+     * dos valores => boolean | true or false
+     * cadenas => char
+     *  */
+    /*
+     * => no primitivas
+     * String | cadenas largas
+     * */
+
+    byte number2 = -127;
+    int number1 = 999999999;
+    long number3 = 5294945844445454549L;
+    float numer4 = 3.14F;
+    double number5 = 9.9D;
+
+    boolean aBoolean1 = true;
+    boolean aBoolean2 = false;
+
+    char aChar1 = 'c';
+
+    String string = "my string";
+
+    public void myString() {
+    }
+}
