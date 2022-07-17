f7986efea17b0c25925fc1a801c75edb81fb5818
ezpheliarmuz-uisrael <67877369+ezpheliarmuz-uisrael@users.noreply.github.com>
2022-07-17 15:30:32 -0500

    Tarea POO

---
+++ src/main/java/com/spacecodee/poo/tarea/AppMain.java
@@ -0,0 +1,5 @@
+package com.spacecodee.poo.tarea;
+
+public class AppMain {
+
+}
---
+++ src/main/java/com/spacecodee/poo/tarea/Tarea.java
@@ -0,0 +1,7 @@
+package com.spacecodee.poo.tarea;
+
+public class Tarea {
+    public static void main ( String[] args ) {
+
+    }
+}
